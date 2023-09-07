23dc971e76e156be09e6c3f2128c8cd14114b2ac
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2023-09-08 00:00:37 +0630

    added icon and home page layout

--- BlazorWebAsm.MissingHistoricalRecord/Program.cs
+++ BlazorWebAsm.MissingHistoricalRecord/Program.cs
@@ -3,11 +3,13 @@ using BlazorWebAsm.MissingHistoricalRecord.Services.Book;
 using BlazorWebAsm.MissingHistoricalRecord.Services.SupabaseModule;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<SupabaseService>();
 builder.Services.AddScoped<BookService>();
