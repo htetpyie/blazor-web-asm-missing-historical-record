1b4015315185c20bc2e13cef24bcd35c9d6914b8
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2023-09-14 00:04:29 +0630

    IN Projress js file adding

---
+++ BlazorWebAsm.MissingHistoricalRecord/Pages/Book/PageBook.razor.js
@@ -0,0 +1,28 @@
+﻿/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.querySelector('#pages');
+
+/* View in fullscreen */
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+        elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+        elem.msRequestFullscreen();
+    }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+    }
+}
