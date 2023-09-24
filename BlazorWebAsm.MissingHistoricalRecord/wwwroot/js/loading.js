ebd32ee94d423fbf07d625b4f00403f11335e3d4
Htet Pyiephyoe Maung <pyiephyoe299@gmail.com>
2023-09-24 21:54:09 +0630

    fixed loading bug

--- BlazorWebAsm.MissingHistoricalRecord/wwwroot/js/loading.js
+++ BlazorWebAsm.MissingHistoricalRecord/wwwroot/js/loading.js
@@ -1,8 +1,11 @@
 window.enableLoading = function () {
     console.log(document.getElementById("loading"))
     $('#loading').css('display','flex');
+    $('body').css('overflow','hidden');
 }
 
 window.disableLoading = function () {
     $('#loading').css('display', 'none');
+    $('body').css('overflow','auto');
+    
 }
